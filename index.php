35a02da289a9070ad9ee7fe286b425562fe37e6b
MattiaManfreda <mattiamanfreda926@gmail.com>
2021-05-17 16:56:03 +0200

    aggiornamento

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
     // Creare una variabile con un paragrafo di testo. Visualizzare a schermo il paragrafo con la relativa lunghezza e sostituire la badword passata in GET con tre *
 
 
-    $paragrafo = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde possimus temporibus recusandae non quidem, earum quas delectus Perite.';
+    $paragrafo = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde possimus temporibus recusandae non quidem, earum quas delectus Maialis.';
 
     $lunghezza = strlen($paragrafo);
 
-    $newParagrafo = str_replace("Perite", "***", $paragrafo);
+    $newParagrafo = str_replace("Maialis", "***", $paragrafo);
 
     echo $newParagrafo . " lunghezza: " . $lunghezza;
 ?>
